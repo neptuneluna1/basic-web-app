248f53b02cc137d10aa9b453cfcd797cdbf2404f
Isabelle Anis <ianis@andrew.cmu.edu>
2025-10-07 10:04:20 -0400

    name

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -7,12 +7,12 @@ export default function QueryProcessor(query: string): string {
     );
   }
 
-  if (query.toLowerCase().includes("name")) {
-    return "Rohan";
-  }
-
   if (query.toLowerCase().includes("andrew id")) {
     return "ianis";
+  }
+
+  if (query.toLowerCase().includes("name")) {
+    return "ianis_fr";
   }
 
   return "";
